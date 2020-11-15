685be7374782243c3582d96a3d3b8c33d09ce648
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2020-11-15 01:56:11 +0100

    * Rearrange project folders * Publish app as a single file deployment * Use NuGet library dependencies

--- src/CasaXpsUtilities.DiamondNeXus2Vamas/App.cs
+++ src/CasaXpsUtilities.DiamondNeXus2Vamas/App.cs
@@ -1,5 +1,4 @@
-﻿namespace CasaXpsUtilities.DiamondNeXus2Vamas
-{
+﻿    using CasaXpsUtilities.DiamondNeXus2Vamas;
     using Pastel;
     using Ultimately;
     using Ultimately.Async;
@@ -13,98 +12,91 @@
     using System.Threading.Tasks;
 
 
-    public class App
+    Console.OutputEncoding = Encoding.UTF8;
+
+
+    var bracketColour = Color.FromArgb(201, 64, 106);
+    var defaultMessagePrefix = $"\r\n{"[".Pastel(bracketColour)}{"CasaXpsUtilities".Pastel(Color.FromArgb(62, 102, 208))}{"]".Pastel(bracketColour)} ";
+
+    var errorMessageTemplate = $"{defaultMessagePrefix}{"ERROR".Pastel(Color.White).PastelBg(Color.FromArgb(222, 54, 26))} {{0}}";
+
+
+    try
     {
-        public static async Task Main(string[] args)
+        var configurationSerializer = Startup.Container.Locate<ConfigurationSerializer>();
+
+        var configurationReadResult = await configurationSerializer.Read().FlatMapAsync(c => c.SomeWhen(_c => _c != null && !string.IsNullOrWhiteSpace(_c.ConversionDefinitionFilepath), "Definitions filepath is empty"));
+
+        var cachedDefinitionsFileInfo = "";
+
+        if (configurationReadResult.HasValue)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-
-
-            var bracketColour = Color.FromArgb(201, 64, 106);
-            var defaultMessagePrefix = $"\r\n{"[".Pastel(bracketColour)}{"CasaXpsUtilities".Pastel(Color.FromArgb(62, 102, 208))}{"]".Pastel(bracketColour)} ";
-
-            var errorMessageTemplate = $"{defaultMessagePrefix}{"ERROR".Pastel(Color.White).PastelBg(Color.FromArgb(222, 54, 26))} {{0}}";
-
-
-            try
-            {
-                var configurationSerializer = Startup.Container.Locate<ConfigurationSerializer>();
-
-                var configurationReadResult = await configurationSerializer.Read().FlatMapAsync(c => c.SomeWhen(_c => _c != null && !string.IsNullOrWhiteSpace(_c.ConversionDefinitionFilepath), "Definitions filepath is empty"));
-
-                var cachedDefinitionsFileInfo = "";
-
-                if (configurationReadResult.HasValue)
-                {
-                    cachedDefinitionsFileInfo = $" or press {"ENTER".Pastel(Color.FromArgb(255, 208, 0))} to reuse the previously used one";
-                }
-
-
-                // Resize window to fit the messages
-
-                Console.Title = nameof(CasaXpsUtilities);
-
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    Console.WindowWidth = Math.Min(Console.LargestWindowWidth, 215);
-                }
-
-                Console.WriteLine($"Please specify the path to the conversion definition file{cachedDefinitionsFileInfo}:");
-
-
-                var readLine = Console.ReadLine();
-
-                string conversionDefinitionFilepath = null;
-
-                if (readLine == "")
-                {
-                    foreach (var (configuration, _) in configurationReadResult)
-                    {
-                        conversionDefinitionFilepath = configuration.ConversionDefinitionFilepath;
-                    }
-
-                    // Move caret up one step and write on the previous line (due to the user having pressed ENTER - looks more neat)
-                    // http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
-
-                    Console.Write("\u001b[1A");
-                }
-                else
-                {
-                    conversionDefinitionFilepath = readLine.Trim('"');
-                }
-
-                //var sw = new Stopwatch();
-                //sw.Start();
-                var conversionResult = await Startup.Container.Locate<ConversionService>().ConvertAndCreateOutputFile(conversionDefinitionFilepath);
-                //sw.Stop();
-                //Console.WriteLine($"Elapsed time: {(double)sw.ElapsedMilliseconds / 1000:#0.000}s");
-                await conversionResult.Match(
-                    some: async cr =>
-                    {
-                        Console.WriteLine($"{defaultMessagePrefix}Generated VAMAS file has been saved to {$"{cr.OutputDirectoryPath}".Pastel(Color.MediumSeaGreen)}{cr.OutputFilename.Pastel(Color.MediumSpringGreen)}.");
-
-                        if (!configurationReadResult.Exists(c => c.ConversionDefinitionFilepath == conversionDefinitionFilepath))
-                        {
-                            await configurationSerializer.SaveAsync(new Configuration(conversionDefinitionFilepath));
-                        }
-                    },
-                    none: e =>
-                    {
-                        Console.WriteLine(errorMessageTemplate, e.Print(" → ".Pastel(Color.WhiteSmoke), 0, s => s.Pastel(Color.FromArgb(230, 44, 34))));
-
-                        return Task.CompletedTask;
-                    }
-                );
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(errorMessageTemplate, "An unexpected error occurred.");
-
-                if (args != null && args.SingleOrNone().Contains("debug")) Console.WriteLine(e);
-            }
-
-            Console.WriteLine("\nPress any key to continue.");
-            Console.ReadKey(true);
+            cachedDefinitionsFileInfo = $" or press {"ENTER".Pastel(Color.FromArgb(255, 208, 0))} to reuse the previously used one";
         }
+
+
+        // Resize window to fit the messages
+
+        Console.Title = nameof(CasaXpsUtilities);
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Console.WindowWidth = Math.Min(Console.LargestWindowWidth, 215);
+        }
+
+        Console.WriteLine($"Please specify the path to the conversion definition file{cachedDefinitionsFileInfo}:");
+
+
+        var readLine = Console.ReadLine();
+
+        string conversionDefinitionFilepath = null;
+
+        if (readLine == "")
+        {
+            foreach (var (configuration, _) in configurationReadResult)
+            {
+                conversionDefinitionFilepath = configuration.ConversionDefinitionFilepath;
+            }
+
+            // Move caret up one step and write on the previous line (due to the user having pressed ENTER - looks more neat)
+            // http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+
+            Console.Write("\u001b[1A");
+        }
+        else
+        {
+            conversionDefinitionFilepath = readLine.Trim('"');
+        }
+
+        //var sw = new Stopwatch();
+        //sw.Start();
+        var conversionResult = await Startup.Container.Locate<ConversionService>().ConvertAndCreateOutputFile(conversionDefinitionFilepath);
+        //sw.Stop();
+        //Console.WriteLine($"Elapsed time: {(double)sw.ElapsedMilliseconds / 1000:#0.000}s");
+        await conversionResult.Match(
+            some: async cr =>
+            {
+                Console.WriteLine($"{defaultMessagePrefix}Generated VAMAS file has been saved to {$"{cr.OutputDirectoryPath}".Pastel(Color.MediumSeaGreen)}{cr.OutputFilename.Pastel(Color.MediumSpringGreen)}.");
+
+                if (!configurationReadResult.Exists(c => c.ConversionDefinitionFilepath == conversionDefinitionFilepath))
+                {
+                    await configurationSerializer.SaveAsync(new Configuration(conversionDefinitionFilepath));
+                }
+            },
+            none: e =>
+            {
+                Console.WriteLine(errorMessageTemplate, e.Print(" → ".Pastel(Color.WhiteSmoke), 0, s => s.Pastel(Color.FromArgb(230, 44, 34))));
+
+                return Task.CompletedTask;
+            }
+        );
     }
-}
+    catch (Exception e)
+    {
+        Console.WriteLine(errorMessageTemplate, "An unexpected error occurred.");
+
+        if (args != null && args.SingleOrNone().Contains("debug")) Console.WriteLine(e);
+    }
+
+    Console.WriteLine("\nPress any key to continue.");
+    Console.ReadKey(true);
